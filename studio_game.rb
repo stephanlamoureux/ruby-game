2df3404d100bf92570a5ed82be2fcf6b166a2c32
Stephan Lamoureux <sa.lamoureux@gmail.com>
2022-12-27 18:12:01 -0500

    init

---
+++ studio_game.rb
@@ -0,0 +1,10 @@
+def say_hello(name, health=100)
+	"I'm #{name.capitalize} with a health of #{health} as of #{time}."
+end
+
+def time
+  current_time = Time.new
+  current_time.strftime("%I:%M:%S")
+end
+
+puts say_hello("larry", 60)

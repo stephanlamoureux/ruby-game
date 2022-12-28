55540c260c19966315d7f1e299284dff92b638af
Stephan Lamoureux <sa.lamoureux@gmail.com>
2022-12-28 16:47:18 -0500

    created player class and some methods

--- studio_game.rb
+++ studio_game.rb
@@ -1,10 +1,37 @@
-def say_hello(name, health=100)
-	"I'm #{name.capitalize} with a health of #{health} as of #{time}."
-end
-
 def time
   current_time = Time.new
   current_time.strftime("%I:%M:%S")
 end
 
-puts say_hello("larry", 60)
+class Player
+	def initialize(name, health=100)
+		@name = name.capitalize
+		@health = health
+	end
+
+	def to_s
+		"I'm #{@name} with a health of #{@health} as of #{time}."
+	end
+
+	def blam
+		@health -= 10
+		puts "#{@name} got blammed!"
+	end
+
+	def w00t
+		@health += 15
+		puts "#{@name} got w00ted!"
+	end
+end
+
+player1 = Player.new("moe")
+player2 = Player.new('larry', 60)
+player3 = Player.new('curly', 125)
+
+puts player1
+puts player2
+puts player3
+player3.blam
+puts player3
+player3.w00t
+puts player3

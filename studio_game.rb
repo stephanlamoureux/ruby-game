8582183896494a22f3793a1fb2b29867de966cff
Stephan Lamoureux <sa.lamoureux@gmail.com>
2022-12-28 17:23:14 -0500

    added attributes

--- studio_game.rb
+++ studio_game.rb
@@ -4,29 +4,40 @@ def time
 end
 
 class Player
-	def initialize(name, health=100)
-		@name = name.capitalize
-		@health = health
-	end
+  attr_reader :health
+  attr_accessor :name
 
-	def to_s
-		"I'm #{@name} with a health of #{@health} as of #{time}."
-	end
+  def initialize(name, health = 100)
+    @name = name.capitalize
+    @health = health
+  end
 
-	def blam
-		@health -= 10
-		puts "#{@name} got blammed!"
-	end
+  def name=(new_name)
+    @name = new_name.capitalize
+  end
 
-	def w00t
-		@health += 15
-		puts "#{@name} got w00ted!"
-	end
+  def to_s
+    "I'm #{@name} with a health of #{@health} and a score of #{score} as of #{time}."
+  end
+
+  def blam
+    @health -= 10
+    puts "#{@name} got blammed!"
+  end
+
+  def w00t
+    @health += 15
+    puts "#{@name} got w00ted!"
+  end
+
+  def score
+    @health += @name.length
+  end
 end
 
 player1 = Player.new("moe")
-player2 = Player.new('larry', 60)
-player3 = Player.new('curly', 125)
+player2 = Player.new("larry", 60)
+player3 = Player.new("curly", 125)
 
 puts player1
 puts player2
@@ -35,3 +46,8 @@ player3.blam
 puts player3
 player3.w00t
 puts player3
+puts player1.health
+player2.name = "Lawrence"
+puts player2.name
+player3.name = "kurly"
+puts player3.name

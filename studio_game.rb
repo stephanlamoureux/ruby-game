4b798693ca2ec8baf5160b9db3f3d805a4f39f55
Stephan Lamoureux <sa.lamoureux@gmail.com>
2022-12-29 17:27:10 -0500

    added Game class

--- studio_game.rb
+++ studio_game.rb
@@ -1,23 +1,15 @@
-def time
-  current_time = Time.new
-  current_time.strftime("%I:%M:%S")
-end
-
 class Player
-  attr_reader :health
-  attr_accessor :name
 
-  def initialize(name, health = 100)
+	attr_accessor :name
+	attr_reader :health
+
+  def initialize(name, health=100)
     @name = name.capitalize
     @health = health
   end
 
-  def name=(new_name)
-    @name = new_name.capitalize
-  end
-
-  def to_s
-    "I'm #{@name} with a health of #{@health} and a score of #{score} as of #{time}."
+	def to_s
+    "I'm #{@name} with a health of #{@health} and a score of #{score}."
   end
 
   def blam
@@ -31,23 +23,58 @@ class Player
   end
 
   def score
-    @health += @name.length
+    @health + @name.length
   end
+
 end
+
+class Game
+
+	attr_reader :title
+
+	def initialize(title)
+		@title = title
+		@players = []
+	end
+
+	def add_player(new_player)
+		@players.push(new_player)
+	end
+
+	def play
+		puts "There are #{@players.size} players in #{@title}: "
+		@players.each do |player|
+			puts player
+		end
+		@players.each do |player|
+			player.blam
+			player.w00t
+			player.w00t
+			puts player
+		end
+	end
+
+end
+
 
 player1 = Player.new("moe")
 player2 = Player.new("larry", 60)
 player3 = Player.new("curly", 125)
 
-puts player1
-puts player2
-puts player3
-player3.blam
-puts player3
-player3.w00t
-puts player3
-puts player1.health
-player2.name = "Lawrence"
-puts player2.name
-player3.name = "kurly"
-puts player3.name
+knuckleheads = Game.new("Knuckleheads")
+puts knuckleheads.title
+knuckleheads.add_player(player1)
+knuckleheads.add_player(player2)
+knuckleheads.add_player(player3)
+knuckleheads.play
+
+player4 = Player.new("alvin")
+player5 = Player.new("simon", 60)
+player6 = Player.new("theodore", 125)
+
+chipmunks = Game.new("Chipmunks")
+puts chipmunks.title
+chipmunks.add_player(player4)
+chipmunks.add_player(player5)
+chipmunks.add_player(player6)
+chipmunks.play

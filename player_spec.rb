ed5969314e02f7d4e7af7c01acb6dee829a2d506
Stephan Lamoureux <sa.lamoureux@gmail.com>
2022-12-31 13:10:39 -0500

    wrote more tests

--- player_spec.rb
+++ player_spec.rb
@@ -2,20 +2,34 @@ require_relative "spec_helper"
 require_relative "player"
 
 describe Player do
-  it "has a capitalized name" do
-    player = Player.new("larry", 150)
+  before { @player = Player.new("larry", 150) }
 
-    player.name.should == "Larry"
+  it "has a capitalized name" do
+    @player.name.should == "Larry"
     #Rspec3 syntax - expect(player.name).to eq("Larry")
   end
 
-  it "has an initial health"
+  it "has an initial health" do
+    @player.health.should == 150
+  end
 
-  it "has a string representation"
+  it "has a string representation" do
+    @player.to_s.should == "I'm Larry with a health of 150 and a score of 155."
+  end
 
-  it "computes a score as the sum of its health and length of name"
+  it "computes a score as the sum of its health and length of name" do
+    @player.score.should == 155
+  end
 
-  it "increases health by 15 when w00ted"
+  it "increases health by 15 when w00ted" do
+    @player.w00t
 
-  it "decreases health by 10 when blammed"
+    @player.health.should == 165
+  end
+
+  it "decreases health by 10 when blammed" do
+    @player.blam
+
+    @player.health.should == 140
+  end
 end
